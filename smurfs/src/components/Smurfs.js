9bf3142fe6b5d0dad0be66b5c12eee9832f14c23
Michael Williams <manicdesigner@gmail.com>
2019-07-22 20:49:26 -0400

    Smurfs + commented out

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -4,15 +4,11 @@ import { connect } from 'react-redux';
 function Smurfs(props) {
   return (
     <div>
-      {props.map.smurfs(smurf => {
-        return (
-          <div>
-            <h2>{props.smurf.name}</h2>
-            <h3>{props.smurf.age}</h3>
-            <h3>{props.smurf.height}</h3>
-          </div>
-        );
-      })}
+      <div>
+        {/* <h2>{smurf.name}</h2> */}
+        {/* <h3>{smurf.age}</h3> */}
+        {/* <h3>{smurf.height}</h3> */}
+      </div>
     </div>
   );
 }
